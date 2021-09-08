f8a8f23cb72f646853e2ff166e7120de31d7851c
Gonçalves, Marcelo <tchelojg@gmail.com>
2021-09-08 20:42:49 -0300

    Initial commit

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import react from "react";
+import reactDom from "react-dom";
+import ReactDom from "react-dom";
+import App from "./App";
+
+reactDom.render(
+    <App/>,
+    document.getElementById('root')
+)
